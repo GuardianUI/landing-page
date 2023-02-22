a7cedba88db47146c6a5c484b0fcb585ab1540e5
ivelin <ivelin.eth@gmail.com>
2023-02-22 20:08:32 +0000

    feat: first refexp demo test completed
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- tests/home-page-refexp.spec.ts
+++ tests/home-page-refexp.spec.ts
@@ -6,50 +6,73 @@ test.describe("Verify GuardianUI.com home page", () => {
   }) => {
     // Setup
     await page.goto("https://www.guardianui.com/");
-    // set browser viewport to a deterministic value
-    // responsive apps may render different views based on screen size
-    await page.waitForSelector("text=GuardianUI");
-
-    // take a screenshot
-    // ...
+    // take a screenshot of current view
+    const img = await page.screenshot({
+      path: "screenshots/refexp-screenshot.png",
+    });
     // send screenshot and refexp to GuardianUI Click model
-    // ...
-    const img = null;
-    // fetch('https://guardianui-ui-refexp-click.hf.space/run/predict')
-    const centerPoint = refexpModelPredict({
+    const centerPoint = await refexpModelPredict({
       refexp: "select GuardianUI logo at the top left",
       screenshot: img,
     });
-    // use predicted coordinates to verify against known data-testid label
-    const refExpFound = await page.evaluate(
-      ([x, y]) => {
-        const element = document.elementFromPoint(x, y);
-        if (!element) {
-          return false;
-        }
-        const testAttribute = element.getAttribute("data-testid");
-        const isJoinCenterButton = "guardianui-logo" === testAttribute;
-        return isJoinCenterButton;
-      },
-      [centerPoint.x, centerPoint.y]
-    );
-    await expect(refExpFound).toBeTruthy();
+    // select component at predicted point
+    const refExpMatched = await elementAtPoint({
+      page,
+      centerPoint,
+      query: { alt: "GuardianUI" }, // test match with a known attribute value
+    });
+    await expect(refExpMatched).toBeTruthy();
   });
 });
 
-function refexpModelPredict({ refexp: String, screenshot: bytes }) {
-  fetch("https://guardianui-ui-refexp-click.hf.space/run/predict", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      data: [
-        screenshot, // "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==",
-        refexp, // "hello world",
-      ],
-    }),
-  })
-    .then((r) => r.json())
-    .then((r) => {
-      let data = r.data;
-    });
+async function refexpModelPredict({ refexp, screenshot }) {
+  const b64img = "data:image/png;base64," + screenshot.toString("base64");
+  console.debug(b64img.substring(0, 100));
+  const response = await fetch(
+    "https://guardianui-ui-refexp-click.hf.space/run/predict",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        data: [
+          b64img, // "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==",
+          refexp, // "select button xyz",
+        ],
+      }),
+    }
+  );
+  if (response.status != 200) {
+    console.error("Failed to fetch RefExp Click API", { response });
+    const responseBodyText = await response.text();
+    console.error({ responseBodyText });
+    return undefined;
+  }
+  console.debug({ response });
+  const rbody = response.body;
+  console.debug({ rbody });
+  const j = await response.json();
+  const data = j.data;
+  // console.debug({ data });
+  return data[1];
+}
+
+async function elementAtPoint({ page, centerPoint, query }) {
+  const vpSize = await page.viewportSize();
+  const cpTranslated = {
+    x: vpSize?.width ? Math.round(centerPoint.x * vpSize?.width) : 0,
+    y: vpSize?.height ? Math.round(centerPoint.y * vpSize?.height) : 0,
+  };
+  console.debug({ centerPoint, cpTranslated });
+  // use predicted coordinates to verify against known data-testid label
+  const match = await page.evaluate(
+    ([cp, query]) => {
+      const element = document.elementFromPoint(cp.x, cp.y);
+      const attr = Object.keys(query)[0];
+      const match = element?.getAttribute(attr) === query[attr];
+      return match;
+    },
+    [cpTranslated, query]
+  );
+  console.debug({ match });
+  return match;
 }
