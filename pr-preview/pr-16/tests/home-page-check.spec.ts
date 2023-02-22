a75d388e08872941fd23a73815e6f988d003f911
ivelin <ivelin@users.noreply.github.com>
2023-02-22 15:27:39 +0000

    Remove preview for PR 16 🛬

--- pr-preview/pr-16/tests/home-page-check.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-
-test.describe("Verify GuardianUI.com home page", () => {
-  test("Should verify that the target page loads", async ({ page }) => {
-    // Setup
-    await page.goto("https://www.guardianui.com/");
-    await page.waitForSelector('text=GuardianUI');
-  });
-});
